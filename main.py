cb5b7af5f9599c9c7f878d4c7012990aa3694571
zizokhd <149091615+zizokhd@users.noreply.github.com>
2026-04-22 18:00:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from telegram import Update, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import CommandHandler, Application, MessageHandler, CallbackQueryHandler, ContextTypes, filters, ConversationHandler
 import os
-
 #token bot
 
 TOKEN = os.getenv("TOKEN")
 
 ADMIN_ID = 7462244340
+ 
 products = {
 
     "Box : ps4 + 2 controller + 3 dvd games":" ملاين 5 ",
@@ -17,19 +17,25 @@ products = {
 #امر البدء start
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data.clear()
+    context.chat_data.clear()
     buttons = [
-        ["المنتجات🛒"],
-        ["طلب"]
+        ["المنتجات🛒","طلب"],
+        ["مساعدة"]
+        
     ]
     keyboard = ReplyKeyboardMarkup(buttons,resize_keyboard=True)
     
     await update.message.reply_text(
         "مرحبا بك في متجرنا ☺️",reply_markup=keyboard
     )
-
+    return ConversationHandler.END
+async def rest_order(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    context.user_data.clear()
 # التعامل مع أزرار القائمة
 async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
+     
     if text == "المنتجات🛒":
         keyboard = [
             [InlineKeyboardButton("Box : ps4 + 2 controller + 3 dvd games", callback_data="box pro")],
@@ -45,6 +51,28 @@ async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
             msg,
             reply_markup=reply_markup
         )
+    elif text == "مساعدة":
+        await update.message.reply_text(
+        "دليل استخدام البوت🤖\n\n"
+        "مرحبًا بك في متجرنا 🛍\n\n"
+        "طريقة استخدام البوت:\n"
+        "1- اضغط زر (المنتجات) لمشاهدة المنتجات المتوفرة.\n"
+        "2- اضغط زر (طلب) لبدء طلب جديد.\n"
+        "3- أدخل معلوماتك بالترتيب:\n"
+        "   • الاسم الكامل\n"
+        "   • رقم الهاتف\n"
+        "   • الولاية\n"
+        "   • العنوان\n"
+        "4- اختر المنتج الذي تريده.\n"
+        "5- سيتم إرسال طلبك مباشرة للإدارة ✅\n\n"
+        "6- لي الغاء الطلب اكتب /cancel ❌\n"
+        "ملاحظة:\n"
+        "رقم الهاتف يجب أن يكون 10 أرقام ويبدأ بـ 05 أو 06 أو 07.\n\n"
+        
+    )
+    
+        
+
         #اسعار المنتجات
 async def prices(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
@@ -56,6 +84,9 @@ async def prices(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if query.data == "box game":
         await query.message.reply_text("450 الف 💵")
 
+
+ 
+      
 
 NAME, PHONE, STATE, ADDRESS, PRODUCT = range(5)
 
@@ -79,10 +110,6 @@ async def get_state(update: Update,context: ContextTypes.DEFAULT_TYPE):
 
 async def get_address(update: Update,context: ContextTypes.DEFAULT_TYPE):
     context.user_data["address"] = update.message.text
-     
-     
-
-
     keyboard = [
         [InlineKeyboardButton("Box : ps4 + 2 controller + 3 dvd games", callback_data="box pro")],
         [InlineKeyboardButton("Box : 2controller + 3 dvd games", callback_data="box minaul")],
@@ -95,6 +122,22 @@ async def get_address(update: Update,context: ContextTypes.DEFAULT_TYPE):
     )
     return PRODUCT
 
+async def cancel_order(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    context.user_data.clear()
+    buttons = [
+        ["المنتجات🛒","طلب"],
+        ["مساعدة"]
+         
+    ]
+    keyboard = ReplyKeyboardMarkup(buttons,resize_keyboard=True)
+    await update.message.reply_text(
+        "تم الغاء الطلب ❌",
+        reply_markup=keyboard
+    )
+    return ConversationHandler.END
+
+    
+
 async def get_product(update: Update,context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
@@ -102,8 +145,9 @@ async def get_product(update: Update,context: ContextTypes.DEFAULT_TYPE):
     product_name = query.data
     context.user_data["product"] = product_name
     buttons = [
-        ["المنتجات🛒"],
-        ["الطلب"]
+        ["المنتجات🛒","الطلب"],
+        ["مساعدة"]
+        
         ]
     keyboard = ReplyKeyboardMarkup(
         buttons,
@@ -145,13 +189,19 @@ conv_handler = ConversationHandler(
         ADDRESS: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_address)],
         PRODUCT: [CallbackQueryHandler(get_product)]
     },
-    fallbacks = []
+    
+    fallbacks=[
+        CommandHandler("start",start),
+        CommandHandler("cancel", cancel_order),
+         
+    ],
+        allow_reentry=True
 )
 
 
 app.add_handler(CommandHandler("start",start))
 app.add_handler(conv_handler)
-app.add_handler(MessageHandler(filters.TEXT,menu))
+app.add_handler(MessageHandler(filters.TEXT, menu))
 app.add_handler(CallbackQueryHandler(prices))
 
 app.run_polling()
